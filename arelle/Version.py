bd333e598876fec0d46c20bc93a2aeae7d8c02ce
Herman Fischer <fischer@markv.com>
2021-09-05 21:16:51 -0700

    cumulative EDGAR 21.3 changes in testing

--- arelle/Version.py
+++ arelle/Version.py
@@ -2,9 +2,9 @@
 This module represents the time stamp when Arelle was last built
 
 @author: Mark V Systems Limited
-(c) Copyright 2020 Mark V Systems Limited, All rights reserved.
+(c) Copyright 2021 Mark V Systems Limited, All rights reserved.
 
 '''
-__version__ = '1.2020.05.10'  # number version of code base and date compiled
-version = '2020-05-10 16:47 UTC'  # string version of date compiled
-copyrightLatestYear = '2020'  # string version of year compiled
+__version__ = '1.2021.09.02'  # number version of code base and date compiled
+version = '2021-09-02 04:37 UTC'  # string version of date compiled
+copyrightLatestYear = '2021'  # string version of year compiled
